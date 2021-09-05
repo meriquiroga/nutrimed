a3a2a22c7cb7878180cd32f91993835b2743e83a
Dario@Rojas <dariourojas07@gmail.com>
2021-09-05 14:37:44 -0300

    indexs

--- routes/index.js
+++ routes/index.js
@@ -1,35 +1,36 @@
-const express = require('express')
-const router = express.Router()
-const passport  = require('passport')
-const userControllers = require('../controllers/userControllers')
-const doctorControllers = require('../controllers/doctorControllers')
-const patientControllers = require('../controllers/patientControllers')
-const appointmentControllers = require('../controllers/appointmentControllers')
+const express = require("express");
+const router = express.Router();
+const passport = require("passport");
+const userControllers = require("../controllers/userControllers");
+const doctorControllers = require("../controllers/doctorControllers");
+const patientControllers = require("../controllers/patientControllers");
+const appointmentControllers = require("../controllers/appointmentControllers");
 
-router.route('/user')
-.post(userControllers.addUser)
+router.route("/user").post(userControllers.addUser);
 
-router.route('/doctor')
-.get(doctorControllers.singIn)
+router.route("/doctor").get(doctorControllers.singIn);
 
-router.route('/doctor/:id')
-.get(doctorControllers.getDoctorById)
-.put(passport.authenticate('jwt',{session:false}),doctorControllers.changedDoctor)//llega el id del doc y por token la info del paciente
-router.route('/doctor/perfil')
-.put(doctorControllers.editPerfil)
+router
+  .route("/doctor/:id")
+  .get(doctorControllers.getDoctorById)
+  .put(
+    passport.authenticate("jwt", { session: false }),
+    doctorControllers.changedDoctor
+  ); //llega el id del doc y por token la info del paciente
+router.route("/doctor/perfil").put(doctorControllers.editPerfil);
 
-router.route('/patient')
-.get(patientControllers.singIn)
-.put(patientControllers.putPatient)
+router
+  .route("/patient")
+  .get(patientControllers.singIn)
+  .put(patientControllers.putPatient);
 
-router.route('/patient/:id')
-.post(patientControllers.addMedicalData)
+router.route("/patient/:id").post(patientControllers.addMedicalData);
 
-router.route('/appointment/:id')
-.post(passport.authenticate('jwt',{session:false}),appointmentControllers.addAppointment)
+router
+  .route("/appointment/:id")
+  .post(
+    passport.authenticate("jwt", { session: false }),
+    appointmentControllers.addAppointment
+  );
 
-module.exports=router
-
-
-
-
+module.exports = router;
