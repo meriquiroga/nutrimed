ae6dae6acfe63860e933d5512b1a3061b4c60afb
unknown <meriquiroga@hotmail.com>
2021-09-05 21:00:40 -0300

    Responsive

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -4,20 +4,20 @@ const Header = () => {
     return (
         <header>
             <div className="navbar">
-            <img src='/assets/logo.png' alt=""/>
-            <ul>
-                <li>
-                <NavLink exact to="/"><p>HOME</p></NavLink>
-                </li>
-                <li>
-                <NavLink to="/signup"><p>CREAR CUENTA</p></NavLink>
-                </li>
-                <li>
-                <NavLink to="/"><p>INGRESAR</p></NavLink>
-                </li>
-            </ul>
+                <img src='/assets/logo.png' alt=""/>
+                <ul>
+                    <li>
+                    <NavLink exact to="/"><p>HOME</p></NavLink>
+                    </li>
+                    <li>
+                    <NavLink to="/signup"><p>CREAR CUENTA</p></NavLink>
+                    </li>
+                    <li>
+                    <NavLink to="/"><p>INGRESAR</p></NavLink>
+                    </li>
+                </ul>
             </div>
-            <button>SOLICITAR TURNOS</button>
+            <button>SOLICITAR TURNO</button>
         </header>
     )
 }
