58ed6462a9d99f62ba4b0d23f5b86a86902c24ac
Carla <carlamalderete@gmail.com>
2021-09-06 20:56:12 -0300

    encarado review

--- frontend/src/redux/reducers/usersReducer.js
+++ frontend/src/redux/reducers/usersReducer.js
@@ -6,21 +6,6 @@ const usersReducer = (state={dataUser:{}, token: null}, action) => {
                 dataUser: action.payload.newUser,
                 token: action.payload.token
             }
-            case "GET_ALL_DOCTORS":
-                return {
-                  ...state,
-                  doctors: action.payload,
-                }
-            case "GET_ONE_DOCTOR":
-                return{
-                    ...state,
-                    doctor:state.doctors.find(obj => obj._id === action.payload)
-                }
-            case "GET_ONE_DOCTOR_DB":
-                return{
-                    ...state,
-                    doctor:action.payload
-                }
         default:
             return state
     }
