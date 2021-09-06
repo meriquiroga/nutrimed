273a8bd865cc1ca37e1bad4509ea988848b755ec
Franco Quattrini <fquattrini01@gmail.com>
2021-09-06 20:52:01 -0300

    xx

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,11 +4,11 @@ import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom"
 import Header from "./components/Header"
 import Footer from "./components/Footer"
 import SignUpPat from "./components/SignUpPat"
-import DoctorInformation from "./components/DoctorInformation"
 import Staff from "./pages/Staff"
 import SignUpDoc from "./components/SignUpDoc"
 import Patient from "./pages/Patient"
 import PatientCompleteData from "./components/PatientCompleteData"
+import Shifts from "./components/Shifts"
 // import { connect } from "react-redux";
 // import {useEffect} from "react"
 
@@ -26,10 +26,10 @@ const App = () => {
             <Route exact path="/" component={Home} />
             <Route path="/signup" component={SignUpPat} />
             <Route path="/staff" component={Staff} />
-            <Route path="/information" component={DoctorInformation} />
             <Route path="/editdoc" component={SignUpDoc} />
             <Route path="/patient" component={Patient} />
             <Route path="/patientdata" component={PatientCompleteData} />
+            <Route path="/shifts" component={Shifts} />
          </Switch>
          <Footer />
       </BrowserRouter>
--- routes/index.js
+++ routes/index.js
@@ -1,37 +1,62 @@
-const express = require("express");
-const router = express.Router();
-const passport = require("passport");
-const userControllers = require("../controllers/userControllers");
-const doctorControllers = require("../controllers/doctorControllers");
-const patientControllers = require("../controllers/patientControllers");
-const appointmentControllers = require("../controllers/appointmentControllers");
+const express = require("express")
+const router = express.Router()
+const passport = require("passport")
+const userControllers = require("../controllers/userControllers")
+const doctorControllers = require("../controllers/doctorControllers")
+const patientControllers = require("../controllers/patientControllers")
+const appointmentControllers = require("../controllers/appointmentControllers")
 
-router.route("/user")
-.post(userControllers.addUser)
+router.route("/user").post(userControllers.addUser)
 
-router.route("/doctor")
-.get(doctorControllers.singIn)
-.put(passport.authenticate("jwt", { session: false }),doctorControllers.editProfile)
+router
+   .route("/doctor")
+   .get(doctorControllers.singIn)
+   .put(
+      passport.authenticate("jwt", { session: false }),
+      doctorControllers.editProfile
+   )
 
-router.route("/doctors")
-.get(doctorControllers.getDoctors)
+router.route("/doctors").get(doctorControllers.getDoctors)
 
-router.route("/doctor/:id")
-.get(doctorControllers.getDoctorById)
-.put(passport.authenticate("jwt", { session: false }),doctorControllers.changedReview)
+router
+   .route("/doctor/:id")
+   .get(doctorControllers.getDoctorById)
+   .put(
+      passport.authenticate("jwt", { session: false }),
+      doctorControllers.changedReview
+   )
 
-router.route("/patient")
-.get(patientControllers.singIn)
-.put(passport.authenticate("jwt", { session: false }),patientControllers.editProfile)
+router
+   .route("/patient")
+   .get(patientControllers.singIn)
+   .put(
+      passport.authenticate("jwt", { session: false }),
+      patientControllers.editProfile
+   )
 
-router.route("/patient/:id")
-.put(passport.authenticate("jwt", { session: false }),patientControllers.addMedicalData)
+router
+   .route("/patient/:id")
+   .put(
+      passport.authenticate("jwt", { session: false }),
+      patientControllers.addMedicalData
+   )
 
-router.route("/appointment/:id")
-.post(passport.authenticate("jwt", { session: false }),appointmentControllers.addAppointment)
-.delete(passport.authenticate("jwt", { session: false }),appointmentControllers.deleteAppointment)
+router
+   .route("/appointment/:id")
+   .post(
+      passport.authenticate("jwt", { session: false }),
+      appointmentControllers.addAppointment
+   )
+   .delete(
+      passport.authenticate("jwt", { session: false }),
+      appointmentControllers.deleteAppointment
+   )
 
-router.route("/appointments")
-.get(passport.authenticate("jwt", { session: false }),appointmentControllers.getAppointments)
+router
+   .route("/appointments")
+   .get(
+      passport.authenticate("jwt", { session: false }),
+      appointmentControllers.getAppointments
+   )
 
-module.exports = router;
+module.exports = router
