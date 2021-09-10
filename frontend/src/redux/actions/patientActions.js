f78c648f8660ffd1fda8b8712ee9381d00890f47
Rodrigo <goitiarodrigo@hotmail.com>
2021-09-10 10:37:26 -0300

    cambio

--- frontend/src/redux/actions/patientActions.js
+++ frontend/src/redux/actions/patientActions.js
@@ -1,19 +1,19 @@
 import axios from "axios";
 
 const patientActions = {
-  editProfilePatient: () => {
-    console.log("editando");
-    return async () => {
-      try {
-        let res = await axios.put(`http://localhost:4000/api/patient`, {});
-        if (res.data.response) {
-          return { success: true, response: res.data.response };
-        }
-      } catch (err) {
-        return { success: false, res: err };
-      }
-    };
-  },
+  // editProfilePatient: () => {
+  //   console.log("editando");
+  //   return async () => {
+  //     try {
+  //       let res = await axios.put(`http://localhost:4000/api/patient`, {});
+  //       if (res.data.response) {
+  //         return { success: true, response: res.data.response };
+  //       }
+  //     } catch (err) {
+  //       return { success: false, res: err };
+  //     }
+  //   };
+  // },
 
   getPatients: () => {
     return async (dispatch, getState) => {
